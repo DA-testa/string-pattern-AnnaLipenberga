5aabfe5cc6726b8d9d23fc08d5874e37ec1ea0ea
AnnaLipenberga <124579305+AnnaLipenberga@users.noreply.github.com>
2023-04-15 14:42:39 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -76,7 +76,5 @@ def get_occurrences(pattern, text):
 
 # this part launches the functions
 if __name__ == '__main__':
-    inputs = read_input()
-    if inputs is not None:
-        print_occurrences(get_occurrences(*read_input()))
+    print_occurrences(get_occurrences(*read_input()))
 
